b1f4a50e32a757fd79dd760b9a979c65cf14973a
HafdhiAtef <60395187+HafdhiAtef@users.noreply.github.com>
2021-10-21 20:42:57 +0100

    Update TimeSheetDevOpsApplication.java

--- src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
+++ src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
@@ -2,8 +2,11 @@ package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class TimeSheetDevOpsApplication {
 	
 	
@@ -11,5 +14,11 @@ public class TimeSheetDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeSheetDevOpsApplication.class, args);
 	}
+	@GetMapping("/")
+    	public String hello()
+    		{
+        	return "Hello From SpringBoot";
+   		 }
+
 
 }
