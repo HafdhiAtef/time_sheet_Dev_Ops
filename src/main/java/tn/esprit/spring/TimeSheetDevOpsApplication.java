f6cc769801a16cdceeebb9b807e6486011f5dada
HafdhiAtef <60395187+HafdhiAtef@users.noreply.github.com>
2021-10-21 18:41:47 +0100

    Update TimeSheetDevOpsApplication.java
    added helloworld !

--- src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
+++ src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimeSheetDevOpsApplication {
-
+	
+	@RequestMapping("/")
+    	String home() {
+        	return "Hello World - v3!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TimeSheetDevOpsApplication.class, args);
 	}
