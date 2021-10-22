e68c05af4e522a92de55a1c1ed867b50174d90d1
HafdhiAtef <atef.hafdhi@esprit.tn>
2021-10-22 15:52:22 +0100

    Added Hello From SpringBoot

--- src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
+++ src/main/java/tn/esprit/spring/TimeSheetDevOpsApplication.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@RestController
 public class TimeSheetDevOpsApplication {
 	
 	
     
 	public static void main(String[] args) {
 		SpringApplication.run(TimeSheetDevOpsApplication.class, args);
+	
+	
 	}
-	@GetMapping("/")
-    	public String hello()
-    		{
-        	return "Hello From SpringBoot";
-   		 }
-
+ 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello From SpringBoot";
+	}
 
 }
