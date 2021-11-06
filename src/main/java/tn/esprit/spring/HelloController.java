4b29d544cc4141af42bb8def13a5d247e189377d
HafdhiAtef <60395187+HafdhiAtef@users.noreply.github.com>
2021-11-06 23:34:46 +0100

    Update HelloController.java

--- src/main/java/tn/esprit/spring/HelloController.java
+++ src/main/java/tn/esprit/spring/HelloController.java
@@ -28,7 +28,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello From SpringBoot";
+		return "Hello From SpringBoot v1";
 	}
 	
 	@GetMapping("/retrieve-users")
